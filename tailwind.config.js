7d3df4bfbd9520e315260d3472c9e99495090a49
Santiago Quipildor <Quipildorsantiago@gmail.com>
2022-10-04 21:57:49 -0300

    #4-setting up colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+		colors: {
+			mainPurple: "#635FC7",
+			mainPurpleHover: "#A8A4FF",
+			darkGrey: "#2B2C37",
+			darkGreySecondary: "#3E3F4E",
+			graySecondary: "#828FA3",
+			Light: "#E4EBFA",
+			veryDarkGrey: "#20212C",
+			lightGrey: "#F4F7FD",
+			mainRed: "#EA5555",
+			mainRedHover: "#FF9898",
+		},
+		fontFamily: {
+			'JakartaSans': 'Plus Jakarta Sans',
+		}
+	},
+	plugins: [],
+};
