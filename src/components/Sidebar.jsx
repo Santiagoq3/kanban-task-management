75793228ba10a85e769a7d85885e809f9678ba87
Santiago Quipildor <Quipildorsantiago@gmail.com>
2022-10-13 20:36:05 -0300

    #11- navbar responsive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,33 +1,61 @@
-export const Sidebar = () => {
+// import { useState } from "react";
+/* eslint-disable react/prop-types */
+export const Sidebar = ({ setIsShowSidebar, isShowSidebar }) => {
   return (
-    <nav className='md:block md:static md:rounded-none absolute  p-6 top-24 flex justify-between flex-col left-24 right-24  rounded-md bg-white  '>
-      <p className='mb-4 font-bold text-graySecondary text-sm tracking-widest uppercase'>
+    <nav className='md:block md:static md:rounded-none md:w-[260px] absolute top-24 flex justify-between flex-col left-24 right-24  rounded-md bg-white  '>
+      <p className='mb-4 pt-6 pl-6 font-bold text-graySecondary text-sm tracking-widest uppercase'>
         All boards (3)
       </p>
       <ul>
         <li className='grid gap-6'>
-          <a className='flex gap-3' href=''>
-            <img src='../assets/icon-board.svg' />
-            <p className='font-semibold text-graySecondary'>Platform Launch</p>
+          <a
+            className='flex  max-w-[250px] rounded-r-xl text-graySecondary hover:text-white  items-center gap-3 hover:bg-mainPurple pl-6 pt-2 pb-2'
+            href=''
+          >
+            <img className='w-[16px] h-[16px]' src='../assets/icon-board.svg' />
+            <p className='font-semibold'>Platform Launch</p>
           </a>
-          <a className='flex gap-3' href=''>
-            <img src='../assets/icon-board.svg' />
-            <p className='font-semibold text-graySecondary'>Marketing plant</p>
+          <a
+            className='flex  max-w-[250px] rounded-r-xl text-graySecondary hover:text-white  items-center gap-3 hover:bg-mainPurple pl-6 pt-2 pb-2'
+            href=''
+          >
+            <img className='w-[16px] h-[16px]' src='../assets/icon-board.svg' />
+            <p className='font-semibold'>Marketing plant</p>
           </a>
-          <a className='flex gap-3' href=''>
-            <img src='../assets/icon-board.svg' />
-            <p className='font-semibold text-graySecondary'>Roadmap</p>
+          <a
+            className='flex  max-w-[250px] rounded-r-xl text-graySecondary hover:text-white  items-center gap-3 hover:bg-mainPurple pl-6 pt-2 pb-2'
+            href=''
+          >
+            <img className='w-[16px] h-[16px]' src='../assets/icon-board.svg' />
+            <p className='font-semibold'>Roadmap</p>
           </a>
-          <a className='flex gap-3' href=''>
-            <img src='../assets/icon-board.svg' />
-            <p className='font-semibold text-mainPurple'>+ Create New Board</p>
+          <a
+            className='flex  max-w-[250px] rounded-r-xl text-mainPurple hover:text-white  items-center gap-3 hover:bg-mainPurple pl-6 pt-2 pb-2'
+            href=''
+          >
+            <img className='w-[16px] h-[16px]' src='../assets/icon-board.svg' />
+            <p className='font-semibold'>+ Create New Board</p>
           </a>
         </li>
       </ul>
-      <div className='p-3 mt-6 flex justify-evenly bg-Light rounded'>
-        <img className='cursor-pointer' src='../assets/icon-light-theme.svg' />
+      <div className='p-2'>
+        <div className='p-3 mt-6 flex justify-evenly bg-Light rounded'>
+          <img
+            className='cursor-pointer'
+            src='../assets/icon-light-theme.svg'
+          />
 
-        <img className='cursor-pointer' src='../assets/icon-dark-theme.svg' />
+          <img className='cursor-pointer' src='../assets/icon-dark-theme.svg' />
+        </div>
+      </div>
+      <div
+        onClick={() => setIsShowSidebar(!isShowSidebar)}
+        className='hidden md:flex items-center gap-2 p-4 cursor-pointer'
+      >
+        <img src='../assets/icon-hide-sidebar.svg' />
+        <p className='text-graySecondary text-base font-semibold'>
+          Hide sidebar
+        </p>
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from '../components/Sidebar';
-
 export const Home = () => {
+  const [isShowSidebar, setIsShowSidebar] = useState(true);
   return (
     <>
       <div className='md:flex h-[890px] m-h-[1024px] bg-mainRed md:bg-mainPurpleHover sm:bg-mainPurple'>
-        <Sidebar />
+        {isShowSidebar && (
+          <Sidebar
+            setIsShowSidebar={setIsShowSidebar}
+            isShowSidebar={isShowSidebar}
+          />
+        )}
         <Outlet />
       </div>
     </>
