3068c91ce22bd6135e0daa5cb21abddb1b2d1c1b
Santiago Quipildor <Quipildorsantiago@gmail.com>
2022-10-17 22:08:09 -0300

    #13 añadiendo boton para abrir la sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,20 @@ export const Home = () => {
   return (
     <>
       <div className='md:flex h-[890px] m-h-[1024px] bg-mainRed md:bg-mainPurpleHover sm:bg-mainPurple'>
-        {isShowSidebar && (
+        {isShowSidebar ? (
           <Sidebar
             setIsShowSidebar={setIsShowSidebar}
             isShowSidebar={isShowSidebar}
           />
+        ) : (
+          <div
+            onClick={() => setIsShowSidebar(!isShowSidebar)}
+            className='absolute bottom-20 left-0  p-4 bg-mainPurple rounded-r-2xl cursor-pointer	hover:bg-mainPurpleHover	'
+          >
+            <img className='w-4 h-3' src='../assets/icon-show-sidebar.svg' />
+          </div>
         )}
+
         <Outlet />
       </div>
     </>
